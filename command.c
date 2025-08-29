7d7dbba7b82d0e85f9e5e4a66c7156075b37a1ef
Joaquin Pairetti <joaquin.pairetti@mi.unc.edu.ar>
2025-08-29 02:05:40 +0000

    Revert "fix: comentarios e identacion"
    This reverts commit 4049cdee49035a17b80b91e26c5d573fff79efe7.

--- command.c
+++ command.c
@@ -6,14 +6,30 @@
 
 #include "command.h"
 
+/* intento crear la estructura para un comando simple,
+ * se usaría una doble lista enlazada (Glist) para 
+ * y dos punteros de tipo char (redirectores)
+ * teniendo en cuenta el ejemplo:
+ * ls -l ej1.c > out < in
+ * 
+ * ls es el primer valor de arg (cmd)
+ * -l = arg 1
+ * ej1.c = arg2
+ * > out escribe el archivo "out"
+ * < in lee el archivo "in"
+ * 
+ * 
+ */
+
+
 struct scommand_s {
-    GList* arg;           // argumentos y comandos
+    GList* arg;   // comando/argumento
     unsigned int length;  // cantidad de argumentos
-    char* input;          // archivo de entrada (si es necesario)
-    char* output;         // archivo de salida (si es necesario)
+    char* input;  // archivo de entrada (si es necesario)
+    char* output; // archivo de salida (si es necesario)
 };
 
- /* GList se define como:
+ /* glist se define como:
  *
  * typedef struct _GList GList;
  * 
@@ -42,35 +58,57 @@ scommand scommand_new(void){
 
 scommand scommand_destroy(scommand self){
     assert(self != NULL);
-
-    g_list_free_full(self->arg, g_free);   // usa g_free para cada arg de glist
+/*
+ * hace falta liberar la memoria de todos los punteros
+ * no basta con hacer g_free(self->arg) porque habría memory leaks
+ * 
+ * uso "g_list_free_full" que libera la memoria de todos
+ * los elementos de glist, en vez de solo el primero 
+ * 
+ * void g_list_free_full (GList* list, GDestroyNotify free_func)
+ * toma un puntero de tipo glist (en este caso self->arg)
+ *
+ */
+    g_list_free_full(self->arg, g_free); // usa g_free para cada arg de glist
     g_free(self->input);                   // además libero el resto de elementos de scommand
     g_free(self->output);
     g_free(self);
     
+   
     return self;
 }
 
 void scommand_push_back(scommand self, char * argument){
     assert(self!=NULL && argument!=NULL);
 
-    // GList* g_list_append (GList* list, gpointer data)
+/* GList* g_list_append (GList* list, gpointer data)
+ * se tiene un puntero de tipo glist (en este caso self->arg)
+ * y un puntero a un dato (en este caso sería argument)
+ * 
+ * entonces busco que se agregue un arg 
+ * al final de la lista ya existente
+ */
 
-    self->arg = g_list_append (self->arg, argument);        // Agrego argument al final de la lista de argumentos
-    self->length = self->length+1;
+ self->arg = g_list_append (self->arg, argument);
+ self->length = self->length+1;
 }
 
 
 void scommand_pop_front(scommand self) {
     assert(self != NULL && !scommand_is_empty(self));
+    //chequeo el invariante o sea si self apunta a un comando y self no es vacio, es decir tiene alguin nodo
     
-    GList *first = self->arg;                               // el nodo a borrar 
-    char *arg0 = first->data;                               // el dato del nodo (ej, "ls" de tipo char*)
-    free(arg0);                                             // liberar el dato que fue alocado dinámicamente
+    GList *first = self->arg;          // el nodo a borrar 
+    char *arg0 = first->data;          // el dato del nodo (ej, "ls" de tipo char*)
 
-    self->arg = g_list_delete_link(self->arg, first);       // libero el nodo, una vez que ya libere el dato alojado dinamicamente
+    // liberar el dato que fue alocado dinámicamente
+    free(arg0);                        //borro ls
 
-    self->length -= 1;                                      // mantener invariante de tamaño
+    // libero el nodo, una vez que ya libere el dato aloojado dinamicamente
+    self->arg = g_list_delete_link(self->arg, first);
+
+    // mantener invariante de tamaño
+    self->length -= 1;
 }
 
 void scommand_set_redir_in(scommand self, char * filename){
@@ -85,33 +123,40 @@ void scommand_set_redir_out(scommand self, char * filename){
 
 bool scommand_is_empty(const scommand self){
     assert(self!=NULL);
-    return self->length == 0;
+return self->length == 0;
 }
+
 
 unsigned int scommand_length(const scommand self){
     assert(self!=NULL);
-    return self->length;
+return self->length;
 }
 
 char * scommand_front(const scommand self){
     assert(self!=NULL && !scommand_is_empty(self));
-    return self->arg->data;
+/*
+* entiendo que se refiere a devolver el primer elemento 
+* siguiendo el cuadro de referencia del principio de command.h
+* se estaría hablando de "cmd"
+* o sea, devuelve self->arg->data
+*/
+return self->arg->data;
 }
 
 char * scommand_get_redir_in(const scommand self){
     assert(self!=NULL);
-    return self->input; 
+return self->input; 
 }
 
 char * scommand_get_redir_out(const scommand self){
     assert(self!=NULL);
-    return self->output;
+return self->output;
 }
 char *scommand_to_string(const scommand self) {
     assert(self != NULL);
 
     if (scommand_is_empty(self)) {
-        char *empty = g_malloc0(1);    // string vacío "\0"
+        char *empty = g_malloc0(1);  // string vacío "\0"
         return empty;
     }
 
@@ -135,7 +180,6 @@ char *scommand_to_string(const scommand self) {
         g_string_append(buf, " < ");// agrego " < " al texto
         g_string_append(buf, in); // agrego el nombre del archivo de entrada
     }
-
     if (out != NULL) {// si hay redirección de salida
         g_string_append(buf, " > ");// agrego " > " al texto
         g_string_append(buf, out);// agrego el nombre del archivo de salida
@@ -144,16 +188,18 @@ char *scommand_to_string(const scommand self) {
     // devuelvo el buffer como char* (libero memoria con  g_free) 
     return g_string_free(buf, FALSE);
 }
-
 ////Pipeline////
 
 /* un pipeline es una secuencia de comandos simples conectados por "|" (pipes),
 * para la estructura se requiere una lista enlzada de comandos simples
 * y un bool que indica si debe esperar que termine para ejecutar el siguiente comando
+*
+*
 */
 
+// estructura del pipeline //
 struct pipeline_s {
-    GList* command;     // lista de comandos simples
+    GList* command;    // lista de comandos simples
     bool wait;          // si es true tengo que esperar
     unsigned int length; 
 };
@@ -200,11 +246,11 @@ void pipeline_pop_front(pipeline self) {
     assert(self!=NULL && !pipeline_is_empty(self));
 
     GList* first = self->command;          // el nodo a borrar 
-    scommand sc = first->data;             // dato del nodo: un scommand
+    scommand sc = first->data;         // dato del nodo: un scommand
 
     scommand_destroy(sc);
 
-    self->command = g_list_delete_link(self->command, first);  // nueva cabeza
+    self->command = g_list_delete_link(self->command, first); // nueva cabeza
     self->length -= 1;
 }
 
@@ -238,7 +284,7 @@ bool pipeline_get_wait(const pipeline self) {
 }
 
 char * pipeline_to_string(const pipeline self) {
-    assert(self != NULL);
+     assert(self != NULL);
 
     if (pipeline_is_empty(self)) {// caso borde: pipeline vacío  devolver string vacío
         char *empty = g_malloc0(1);  // devuelve "\0"
@@ -267,3 +313,5 @@ char * pipeline_to_string(const pipeline self) {
     //  devolver char* plano (liberable con g_free por el caller)
     return g_string_free(buf, FALSE);
 }
+
+
