edc91ebafefb1ddee7370a41aa8e19f13c9ee11c
Joaquin Pairetti <joaquin.pairetti@mi.unc.edu.ar>
2025-09-04 18:41:25 +0000

    feat: parse_command, implementacion parcial de parse_pipeline

--- parsing.c
+++ parsing.c
@@ -6,35 +6,61 @@
 #include "command.h"
 
 static scommand parse_scommand(Parser p) {
-    /* Devuelve NULL cuando hay un error de parseo */
-    return NULL;
+    scommand sc = scommand_new();
+    bool error = false;             // corresponde a si da error en el parseo (algo invalido)
+    bool finish = false;            // corresponde a si se encontro un (|) o (\n) 
+    bool have_word = false;         // corresponde a si en el comando hay al menos una palabra
+    
+    while(!error && !finish){
+        arg_kind_t kind;
+        char* arg = parser_next_argument(p, &kind);
+        
+        if (arg == NULL) {                              // caso 1) chequeo si estoy en (|) o en (\n)
+            finish = true;
+        } 
+        else if (kind == ARG_NORMAL) {                  // caso 2) encontre una palabra normal -> agrego a mi sc ese arg
+            scommand_push_back(sc, arg);
+            have_word = true;
+        }
+        else if (kind == ARG_INPUT) {                   // caso 3) encontre un redireccion de entrada (<) -> agrego en mi sc un input
+            if (scommand_get_redir_in(sc) != NULL) { 
+                error = true;
+                free(arg);
+            } else {scommand_set_redir_in(sc, arg);}
+        }
+        else if (kind == ARG_OUTPUT) {                  // caso 4) encontre una redireccion de salida (>) que debo hacer? -> agrego en mi sc un output 
+            if (scommand_get_redir_out(sc) != NULL){
+                error = true;
+                free(arg);
+            } else {scommand_set_redir_out(sc, arg);}
+        }
+    }
+
+    if (error == true || have_word == false) {          // una vez terminado el ciclo chequeo si salto algun error
+        sc = scommand_destroy(sc);
+    }
+
+    return sc;
 }
 
 pipeline parse_pipeline(Parser p) {
     pipeline result = pipeline_new();
     scommand cmd = NULL;
-    bool error = false, another_pipe=true;
+    bool error = false;
+    bool another_pipe = true;
 
     cmd = parse_scommand(p);
-    error = (cmd==NULL); /* Comando inválido al empezar */
+    error = (cmd==NULL);                                // Comando inválido al empezar
     while (another_pipe && !error) {
-        /*
-         * COMPLETAR
-         *
-         */
+        pipeline_push_back(result, cmd);
+        parser_op_pipe(p, &another_pipe);               // tengo que ver si tengo otro pipe
+        //scommand_destroy(cmd); IMPORTANTE CONSULTAR ESTO 
+        if (another_pipe) {
+            cmd = parse_scommand(p);
+            error = (cmd==NULL);
+        }
     }
-    /* Opcionalmente un OP_BACKGROUND al final */
-    }
-    /*
-     *
-     * COMPLETAR
-     *
-     */
 
-    /* Tolerancia a espacios posteriores */
-    /* Consumir todo lo que hay inclusive el \n */
-    /* Si hubo error, hacemos cleanup */
-
-    return NULL; // MODIFICAR
+    return result;
 }
 
