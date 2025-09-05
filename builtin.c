69b02d5f6a903b2c200879e9af67a44c4d5405ee
Franco Galassi <fragalassi@mi.unc.edu.ar>
2025-09-05 18:01:52 -0300

    fix: parenthesis errors

--- builtin.c
+++ builtin.c
@@ -22,16 +22,16 @@ return(builtin_alone(p) == pipeline_length(p) == 1 && builtin_is_internal(pipeli
 
 
 void builtin_run(scommand cmd){
-    assert{builtin_is_internal(cmd);
+    assertbuiltin_is_internal(cmd);
 
-        if (strcmp(scommand_front(cmd), "help") {
+        if (strcmp(scommand_front(cmd), "help")) {
             printf("Shell "MyBash"\n");
             printf("integrantes:\n\n Bosque Lissandro\n Galassi Franco \n Ortega Maximo\n Pairetti Joaquín");
             printf("Comandos:\n\n"cd" = cambia el directorio actual al espeficado\n"help" = imprime comandos disponibles y nombres de los autores\n"exit" = termina el proceso");
 
-       } else if (strcmp(scommand_front(cmd), "exit") {
+       } else if (strcmp(scommand_front(cmd), "exit")) {
         exit(EXIT_SUCCESS);
-        } else if (strcmp(scommand_front(cmd), "cd") {
+        } else if (strcmp(scommand_front(cmd), "cd")) {
             char* comando = NULL;
             if (scommand_length(cmd)==2){
                 comando = g_list_nth_data(cmd->arg, 1)
