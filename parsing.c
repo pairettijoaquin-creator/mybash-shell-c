d1096346d06a969e176d784829ec82c140b58145
Maximo Ortega Oviedo <maximoortega084@gmail.com>
2025-09-08 04:48:36 +0000

    fix:parse_scommad

--- parsing.c
+++ parsing.c
@@ -6,62 +6,73 @@
 #include "command.h"
 
 static scommand parse_scommand(Parser p) {
-    scommand sc = scommand_new();
-    bool error = false;             // corresponde a si da error en el parseo (algo invalido)
-    bool finish = false;            // corresponde a si se encontro un (|) o (\n) 
-    bool have_word = false;         // corresponde a si en el comando hay al menos una palabra, no importa el lugar, es una sintaxis valida para la shell
-    
-    while(!error && !finish){
+    scommand sc = NULL;
+    bool error = false;
+    bool finish = false;
+    bool have_word = false;
+
+    while (!error && !finish) {
         arg_kind_t kind;
-        char* arg = parser_next_argument(p, &kind);
-        
-        // en arg tengo una palabra o redireccion, kind me indica de que tipo es
-        // ahora debo ver por casos que es lo que tomo parser_next_argument(p, &kind)
-        
-        if (arg == NULL) {                              // caso 1) chequeo si estoy en (|) o en (\n)
+        char *arg = parser_next_argument(p, &kind);
+
+        if (arg == NULL) {
+            // Encontró '|' o '\n' y NO los consumió: acá termina este scommand
             finish = true;
-        } 
-        else if (kind == ARG_NORMAL) {                  //caso 2) encontre una palabra normal -> agrego a mi sc ese arg
+        } else if (kind == ARG_NORMAL) {
+            if (sc == NULL) {sc = scommand_new();}
             scommand_push_back(sc, arg);
             have_word = true;
-        }
-        else if (kind == ARG_INPUT) {                   //caso 3) encontre un redireccion de entrada (<) -> agrego en mi sc un input
-            if (scommand_get_redir_in(sc) != NULL) {    // si ya habia redireccion de entrada, error
-                error = true;
-                free(arg);
-            } else {scommand_set_redir_in(sc, arg);};   // si no, lo seteo
-        }
-        else if (kind = ARG_OUTPUT) {                   //caso 4) encontre una redireccion de salida (>) que debo hacer? -> agrego en mi sc un output 
-            if (scommand_get_redir_out(sc) != NULL) {   // si ya habia redireccion de salida, error
-                error = true;
-                free(arg);
-            } else {scommand_set_redir_out(sc, arg);};  // si no, lo seteo
+        } else if (kind == ARG_INPUT) {
+            if (sc == NULL) { 
+                error = true; free(arg); 
+            }
+            else if (scommand_get_redir_in(sc) != NULL) {
+                 error = true; free(arg); 
+                }
+            else { 
+                scommand_set_redir_in(sc, arg); 
+            }
+        } else if (kind == ARG_OUTPUT) {
+            if (sc == NULL) {
+                 error = true; free(arg); 
+                }
+            else if (scommand_get_redir_out(sc) != NULL) {
+                 error = true; free(arg); 
+                }
+            else {
+                 scommand_set_redir_out(sc, arg); 
+                }
         }
     }
 
-    if (error == true || have_word == false) {          // una vez terminado el ciclo chequeo si salto algun error
-        sc = scommand_destroy(sc);
+    if (error || !have_word) {
+        if (sc) sc = scommand_destroy(sc);
+        return NULL;
     }
-
     return sc;
 }
 
+
 pipeline parse_pipeline(Parser p) {
+    assert(p != NULL);
+    assert(! parser_at_eof (p));
     pipeline result = pipeline_new();
     scommand cmd = NULL;
     bool error = false, another_pipe=true;
-
+    
     cmd = parse_scommand(p);
-    error = (cmd==NULL); 
-   while (another_pipe && !error) {
-    pipeline_push_back(result, cmd);// 1) guardar el comando actual en el pipeline 
-    parser_op_pipe(p, &another_pipe);  // 2) ver si hay otro '|' y consumirlo si existe 
 
-    if (another_pipe) { // 2) ver si hay otro '|' y consumirlo si existe
-        cmd = parse_scommand(p);
-        error = (cmd == NULL);  // pipe sin comando a la derecha => error 
-        } 
-   }
+    error = (cmd==NULL); 
+    while (another_pipe && !error) {
+        pipeline_push_back(result, cmd);        // 1) guardar el comando actual en el pipeline 
+        parser_op_pipe(p, &another_pipe);       // 2) ver si hay otro '|' y consumirlo si existe 
+
+        if (another_pipe) {                     // 2) ver si hay otro '|' y consumirlo si existe
+            cmd = parse_scommand(p);
+            error = (cmd == NULL);              // pipe sin comando a la derecha => error 
+        }   
+    }
+    
     parser_skip_blanks(p);
     bool background = false; 
     parser_op_background(p, &background);
@@ -78,6 +89,6 @@ pipeline parse_pipeline(Parser p) {
     if (error || pipeline_length(result) == 0) {
         result = pipeline_destroy(result);
     }
-return result;
-
+    
+    return result;
 }
